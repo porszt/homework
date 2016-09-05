a9be3b8f5961127a3719adccf356b20cf53b3827
Paweł Orszt <pawel.orszt@gmail.com>
2016-09-05 20:03:02 +0200

    CardTester done

---
+++ src/CardTester.java
@@ -0,0 +1,13 @@
+
+public class CardTester {
+
+    public static void main(String[] args) {
+
+        Card card1 = new Card(1, 11);
+        Card card2 = new Card(3, 1);
+
+
+        System.out.println(card1.toString());
+        System.out.println(card2.toString());
+    }
+}
